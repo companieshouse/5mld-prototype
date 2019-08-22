676e3f94200072d2c9b7fb5d23eba621f90fb5cb
Michael Gearon <mgearon@companieshouse.gov.uk>
2019-08-22 11:21:46 +0100

    Updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Obliged Entity PSC Discrepancy',
 
   // Default port that prototype runs on
   port: '3000',
