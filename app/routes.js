d021920c2d2fce6c1b5e80057cb05b9f4c740137
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-02-03 19:26:25 +0000

    Validation

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,97 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/', function (req, res) {
+  res.render('index', {
+  })
+})
+
+router.get('/obliged-entity-type', function (req, res) {
+  res.render('obliged-entity-type', {
+  })
+})
+
+router.get('/obliged-entity-name', function (req, res) {
+  res.render('obliged-entity-name', {
+  })
+})
+
+router.post('/obliged-entity-details-name', function (req, res) {
+  var errors = []
+  if (req.session.data['full-name'] === '') {
+    errors.push({
+      text: 'Enter your name',
+      href: '#full-name'
+    })
+    res.render('obliged-entity-details-name', {
+      errorName: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('obliged-entity-details-telephone')
+  }
+})
+
+router.get('/obliged-entity-organisation', function (req, res) {
+  res.render('obliged-entity-organisation', {
+  })
+})
+
+router.post('/obliged-entity-details-organisation', function (req, res) {
+  var errors = []
+  if (req.session.data['your-organisation-name'] === '') {
+    errors.push({
+      text: 'Enter an organisation name',
+      href: '#your-organisation-name'
+    })
+    res.render('obliged-entity-details-organisation', {
+      errorTelephoneNumber: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('obliged-entity-details-name')
+  }
+})
+
+router.get('/obliged-entity-details-telephone', function (req, res) {
+  res.render('obliged-entity-details-telephone', {
+  })
+})
+
+router.get('/discrepeancy-details/company-number', function (req, res) {
+  res.render('/discrepeancy-details/company-number', {
+  })
+})
+
+router.post('/discrepancy-details/company-number', function (req, res) {
+  var errors = []
+  if (req.session.data['company-number'] === '') {
+    errors.push({
+      text: 'Enter company number',
+      href: '#company-number'
+    })
+    res.render('discrepancy-details/company-number', {
+      errorNum: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/discrepancy-details/psc-names')
+  }
+})
+
+router.get('/discrepeancy-details/psc-names', function (req, res) {
+  res.render('/discrepeancy-details/psc-names', {
+  })
+})
+
+router.get('/discrepeancy-details/other-info', function (req, res) {
+  res.render('/discrepeancy-details/other-info', {
+  })
+})
+
+router.get('/confirmation', function (req, res) {
+  res.render('confirmation', {
+  })
+})
 
 module.exports = router
